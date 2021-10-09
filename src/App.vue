82a1af68128c5c064d3efb5643f496bd1adef547
Mod8124 remote <denisenriquebaca@gmail.com>
2021-10-08 19:42:02 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <Card theme="first">
+    <template v-slot:img>
+       <img src="./assets/icon-sedans.svg" alt="icon-luxury">
+    </template>
+    <template v-slot:title>
+      sedands
+    </template>
+
+      <template v-slot:text>
+       Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
+        or on your next road trip.
+    </template>
+  </Card>
+
+   <Card theme="second">
+       <template v-slot:img>
+       <img src="./assets/icon-suvs.svg" alt="icon-suvs">
+    </template>
+    <template v-slot:title>
+      SUVs
+    </template>
+
+      <template v-slot:text>
+      Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation 
+       and off-road adventures.
+    </template>
+   </Card>
+    <Card theme="third">
+            <template v-slot:img>
+       <img src="./assets/icon-luxury.svg" alt="icon-suvs">
+    </template>
+    <template v-slot:title>
+      Luxury
+    </template>
+
+      <template v-slot:text>
+     Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
+       rental and arrive in style.
+    </template>
+    </Card>
+
+    <section class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Denis B.</a>.
+  </section>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/CarCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Card
+  },
+  data () {
+    return {
+      img:['./assets/icon-luxury.svg', '../assets/icon-luxury.svg', '../assets/icon-luxury.svg']
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import '../src/assets/css/style.css';
+
+.sale {
+    background: fuchsia;
+    color:white;
 }
+
 </style>
---
+++ src/components/CarCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card" :class="{first: theme === 'first', second: theme === 'second', third: theme === 'third'}">
+            <div class="img">
+               <slot name="img"></slot>
+            </div>
+
+            <div class="tittle">
+                 <h3>
+                     <slot name="title"></slot>
+                </h3>
+            </div>
+
+
+            <div class="text">
+                <p>
+                    <slot name="text"></slot>
+                </p>
+            </div>
+
+        <div class="button">
+            <button :class="{colorFirst: theme === 'first', colorSecond : theme === 'second', colorThird : theme === 'third'}">Learn More</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['theme'],
+}
+</script>
